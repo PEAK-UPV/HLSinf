d21f2c04e27c65a7feaf20255df0e2ea157990b0
Jose Flich <jflich@disca.upv.es>
2021-03-12 08:37:29 +0000

    added project files

---
+++ project/HLSinf/AlveoU200/script.tcl
@@ -0,0 +1,32 @@
+############################################################
+## This file is generated automatically by Vitis HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2020 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project HLSinf
+set_top k_conv2D
+add_files ../src/write.cpp
+add_files ../src/winograd_convolution.cpp
+add_files ../src/serialization.cpp
+add_files ../src/relu.cpp
+add_files ../src/read.cpp
+add_files ../src/padding.cpp
+add_files ../src/mul.cpp
+add_files ../src/k_conv2D.cpp
+add_files ../src/join_split.cpp
+add_files ../src/dws_convolution.cpp
+add_files ../src/direct_convolution.cpp
+add_files ../src/cvt.cpp
+add_files ../src/conv2D.h
+add_files ../src/add.cpp
+add_files -tb ../src/test_conv2D.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+open_solution "AlveoU200" -flow_target vitis
+set_part {xcu200-fsgd2104-2-e}
+create_clock -period 3.33 -name default
+config_interface -default_slave_interface s_axilite -m_axi_alignment_byte_size 64 -m_axi_latency 64 -m_axi_max_widen_bitwidth 512
+config_rtl -register_reset_num 3
+#source "./HLSinf/AlveoU200/directives.tcl"
+csim_design -setup
+csynth_design
+cosim_design
+export_design -format ip_catalog
