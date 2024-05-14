1475003b6e555157874cfd704d801009fbb2a7f2
laumecha <laumecha@inf.upv.es>
2024-05-14 10:29:01 +0200

    mem: fixing MEM_1Kx16

--- RTLinf/src/design/mem.v
+++ RTLinf/src/design/mem.v
@@ -739,7 +739,7 @@ module MEM_1Kx16 #(
 );
 
   BRAM_SDP_MACRO #(
-    .BRAM_SIZE     ( "36Kb"    ), // Target BRAM, "18Kb" or "36Kb" 
+    .BRAM_SIZE     ( "18Kb"    ), // Target BRAM, "18Kb" or "36Kb" 
     .DEVICE        ( "7SERIES" ), // Target device: "7SERIES" 
     .WRITE_WIDTH   ( 16        ), // Valid values are 1-72 (37-72 only valid when BRAM_SIZE="36Kb")
     .READ_WIDTH    ( 16        ), // Valid values are 1-72 (37-72 only valid when BRAM_SIZE="36Kb")
@@ -755,9 +755,9 @@ module MEM_1Kx16 #(
     .RDADDR        ( addr_read          ), // Input read address, width defined by read port depth
     .RDCLK         ( clk                ), // 1-bit input read clock
     .RDEN          ( read               ), // 1-bit input read port enable
-    .REGCE         ( 0                  ), // 1-bit input read output register enable
+    .REGCE         ( 1'b0                  ), // 1-bit input read output register enable
     .RST           ( ~rst               ), // 1-bit input reset
-    .WE            ( 4'b1111            ), // Input write enable, width defined by write port depth
+    .WE            ( 2'b11              ), // Input write enable, width defined by write port depth
     .WRADDR        ( addr_write         ), // Input write address, width defined by write port depth
     .WRCLK         ( clk                ), // 1-bit input write clock
     .WREN          ( write              )  // 1-bit input write port enable
